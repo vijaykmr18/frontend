ea8e00c189200e4b05f277ea7b5c164112fef2e5
unknown <guggarivijay016@gmail.com>
2026-06-10 23:04:23 +0530

    redeploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL || '/api';
+const API_BASE =
+  import.meta.env.VITE_API_BASE_URL ||
+  'https://ecommerce-backend-api-4.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE,
